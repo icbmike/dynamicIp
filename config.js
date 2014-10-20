ff65d8504e08ef7e50aa1d3826a46323bfd41b3f
Michael Little <bio595@gmail.com>
2014-10-20 22:41:10 +1300

    Add merge.js. Add lodash requirement

--- config.js
+++ config.js
@@ -1,5 +1,19 @@
 //This file loads config
+var fs = require('fs'),
+	merge = require('./merge'),
+	configfileName = 'config.json';
 
-module.exports = {
-	checkIpInterval = 5000
+var defaults = {
+	checkIpInterval : 5000
 };
+
+
+var configFromFile = {};
+
+if(fs.existsSync(configFileName)){
+	configFromFile = JSON.Parse(fs.readFileSync(configFileName, {encoding: 'utf8'});
+}
+
+var mergedConfig = merge(defaults, configFromFile);
+
+module.exports = mergedConfig;
